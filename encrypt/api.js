3a01d39a249d089abfbb1c7836b4bbbe6cc8ba38
worldtech4ever <77576951+MULTI-GAMER-2@users.noreply.github.com>
2022-03-25 22:06:05 +0000

    Update api.js

--- encrypt/api.js
+++ encrypt/api.js
@@ -3,7 +3,7 @@ var ObfuscateCode = document.querySelector("#ObfuscateCode");
 
 function Obfuscate(){
 
-    var EncodeString=ObfuscateCode.value || ObfuscateCode;
+    var EncodeString=ObfuscateCode;
     var EncodedString = "";
     for (let i=0; i<EncodeString.length; i++){
         
